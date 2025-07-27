0abb8c9d1a87741ff77bd25160f257aaa2a475d2
Semyon <zhigunovsemen@gmail.com>
2025-07-27 17:04:40 +0300

    Заложил класс клиента

---
+++ src/client.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+namespace skipass {
+
+class Client {
+public:
+	enum class gender {
+		male,
+		female
+	};
+
+private:
+	std::string m_name;
+	int8_t m_age;
+	gender m_gender;
+
+public:
+	Client(std::string_view name, int8_t age, gender gender)
+		: m_name(name), m_age(age), m_gender(gender)
+	{
+		if (m_age <= 0)
+			throw std::logic_error{"Неправильно указан возраст!"};
+	}
+
+	/*Запрет на дублирование клиента*/
+	Client(Client const &) = delete;
+
+	Client(Client && o)
+		: m_name(o.m_name), m_age(o.m_age), m_gender(o.m_gender)
+	{
+	}
+
+	Client & operator=(Client && o)
+	{
+		m_name = o.m_name, m_gender = o.m_gender, m_age = o.m_age;
+
+		return *this;
+	}
+
+	Client & operator=(Client const &) = delete;
+
+	std::string_view name() const { return m_name; }
+
+	int8_t age() const noexcept { return m_age; }
+
+	gender gender() const noexcept { return m_gender; }
+};
+
+} // namespace skipass

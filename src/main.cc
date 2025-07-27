244b3653832f93634ae29460746b8ba8cbb2e52f
Semyon <zhigunovsemen@gmail.com>
2025-07-27 15:18:06 +0300

    Ловлю исключения вокруг main()

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,15 @@
+#include <cstdlib>
+#include <exception>
 #include <print>
 
-int main ()
-{
+int main()
+try {
 	std::println("Hello World");
+	return EXIT_SUCCESS;
+} catch (std::exception const & e) {
+	std::println(stderr, "Исключение с сообщением: {}", e.what());
+	return EXIT_FAILURE;
+} catch (...) {
+	std::println(stderr, "Неизвестное исключение");
+	return EXIT_FAILURE;
 }

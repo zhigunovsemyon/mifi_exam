54d6734dea211209c2611b4a10d3441993d8b5c4
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-07-30 12:21:09 +0300

    Проверил на работу open()/close(), работает, память освобождается

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include "client.h"
-#include "timelimitedticket.h"
 #include "ridelimitedticket.h"
+#include "ticketstorage.h"
+#include "timelimitedticket.h"
+#include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <exception>
 #include <memory>
@@ -8,18 +11,33 @@
 
 int main()
 try {
-	auto x = std::make_shared<skipass::Client>(skipass::Client{"s",1,skipass::Client::gender::female});
-	skipass::Ticket::RideLimited tl{x};
-	std::print("remainder:{}\n", tl.remainder());
+	skipass::TicketStorage st;
+	for (int i = 0; i < 100; ++i) {
+		auto x = std::make_shared<skipass::Client>("s", 8, skipass::Client::gender::male);
+		std::shared_ptr<skipass::Ticket::Base> t;
+		if (i % 2)
+			t = std::make_shared<skipass::Ticket::RideLimited>(x);
+		else
+			t = std::make_shared<skipass::Ticket::TimeLimited>(x);
 
-	auto exch = tl.refill(200);
-	std::print("remainder:{}, exchange:{}\n", tl.remainder(), exch);
+		st.insert(t);
+		if (i % 3)
+			st.close(*t->cell_id());
 
-	exch = tl.refill(320);
-	std::print("remainder:{}, exchange:{}\n", tl.remainder(), exch);
+	}
 
-	exch = tl.refill(720);
-	std::print("remainder:{}, exchange:{}\n", tl.remainder(), exch);
+	// std::ranges::for_each(st.m_storage, [&](skipass::TicketStorage::TicketCell const & c) {
+	// 	auto ticket = c.m_ptr.get();
+	// 	auto cell_id_from_ticket = *ticket->cell_id();
+	// 	assert(cell_id_from_ticket == c.m_cellid);
+	//
+	// 	std::print("cell_id: {}, ", c.m_cellid);
+	// 	std::print("ticket_id: {} ", ticket->ticket_id());
+	// 	std::print("Остаток: {}", dynamic_cast<skipass::Ticket::Expirable *>(ticket)->remainder());
+	// 	auto open = *st.is_open(cell_id_from_ticket);
+	// 	std::print(" Открыт?: {}", open);
+	// 	std::println();
+	// });
 	return EXIT_SUCCESS;
 } catch (std::exception const & e) {
 	std::println(stderr, "Исключение с сообщением: {}", e.what());

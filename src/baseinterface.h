50643e904c6d3491260107dc3f9c0d369916a8fd
Semyon <zhigunovsemen@gmail.com>
2025-07-30 21:06:23 +0300

    Начал вирт интерфейс

---
+++ src/baseinterface.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "client.h"
+#include "program.h"
+#include <cstddef>
+#include <memory>
+#include <optional>
+
+namespace skipass::Interface {
+
+class Base {
+	// показать число пользователей
+	// добавить пользователя
+	// удалить пользователя
+	// сменить
+	// купить билет
+	// меню
+
+public:
+	using client_t = std::shared_ptr<Client>;
+	~Base() = default;
+
+	virtual Program::Mode options() const = 0;
+
+	virtual void show_user_count(size_t count) const = 0;
+
+	virtual void invalid_user() const = 0;
+
+	virtual void show_user_info(client_t) const = 0;
+
+	virtual std::optional<client_t> retrieve_user_info() const = 0;
+
+	virtual void print_error_message(std::string_view msg) const = 0;
+};
+
+} // namespace skipass::Interface
